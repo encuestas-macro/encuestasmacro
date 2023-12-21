8eaff5902cf8f0db012b26de20f1239590b5e97f
Johan Rosa <johan.rosaperez@gmail.com>
2023-12-21 09:07:04 -0400

    git commit -m "Add crear_mes function and tests"

--- tests/testthat/test-crear_mes.R
+++ tests/testthat/test-crear_mes.R
@@ -1,9 +1,39 @@
+months_text <-     c(
+  "Enero",
+  "Febrero",
+  "Marzo",
+  "Abril",
+  "Mayo",
+  "Junio",
+  "Julio",
+  "Agosto",
+  "Septiembre",
+  "Octubre",
+  "Noviembre",
+  "Diciembre"
+)
+
+months_shorttext <- stringr::str_sub(months_text, end = 3)
 
 test_that("crear_mes works well", {
-  expect_equal(crear_mes("enero"), 1)
   expect_equal(
     crear_mes(1:12, "number_to_text"),
-    c("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
-      "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre")
+    months_text
+  )
+  expect_equal(
+    crear_mes(months_text, "text_to_number"),
+    1:12
+  )
+  expect_equal(
+    crear_mes(1:12, "number_to_shorttext"),
+    months_shorttext
+  )
+  expect_type(
+    crear_mes(months_text, "text_to_number"),
+    "double"
+  )
+  expect_type(
+    crear_mes(1:12, "number_to_text"),
+    "character"
   )
 })
