1da018e3a2fe816233aa473e9df9c4d0c7c41bc5
Johan Rosa <johan.rosaperez@gmail.com>
2023-12-22 12:08:49 -0400

    Add get_data_eem

--- R/global_variables.R
+++ R/global_variables.R
@@ -1,13 +1,29 @@
+
+#' Read details form yaml file
+#'
+#' @param group
+#'
+#' @return a list with the existing values
+read_details_file <- function(group) {
+  yaml::read_yaml(system.file("details.yaml", package = "encuestasmacro"))[[group]]
+}
+
+eem_details <- read_details_file("eem")
+
+
 utils::globalVariables(
   c(
     "ano",
     "crecimiento_interanual",
     "dia",
     "fecha",
+    "grupo",
+    "horizonte",
     "inflacion_diciembre_obs",
     "ipc_vi",
     "ipc_vm",
     "mes",
+    "name",
     "partida",
     "periodo",
     "tc_diciembre_obs",
@@ -17,6 +33,9 @@ utils::globalVariables(
     "tpm_mes_obs",
     "tpm_trimestre_obs",
     "trimestre",
+    "value",
+    "variable",
+    "variable_label",
     "venta",
     "year"
   )
