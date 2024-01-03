a4ce0a3d16be66e153565eba9e31202661185575
Johan Rosa <johan.rosaperez@gmail.com>
2024-01-03 13:32:09 -0400

    EEM data

--- R/eem_data.R
+++ R/eem_data.R
@@ -2,7 +2,7 @@
 #'
 #' @param data_path an optional argument, needed when your are using the package
 #' outside the project it was build to work on
-#' @param format an string indicating the data frame format "wide" or "long"
+#' @param format a string indicating the data frame format "wide" or "long"
 #'
 #' @return a data frame with the historical data
 #' @export
@@ -56,4 +56,61 @@ get_data_eem <- function(format = c("wide", "long"), data_path = NULL) {
   }
 
   tibble::as_tibble(wide)
+}
+
+#' Forecast for the end of the year
+#'
+#' In the survey forecast for the end of a given year are collected up to 24 times.
+#' 12 times the year before, as end of next year forecasts, and 12 times during the
+#' current year ad en of year expectation. This functions gather those forecasts
+#' into one series
+#'
+#' @param data_eem eem data in wide format
+#' @param variable variable key: "inflacion", "tc", "tcd", "pib" or "tpm"
+#' @param year_to_plot year to plot
+#'
+#' @return a data frame
+#' @export
+#'
+#' @examples
+#' eem_data_diciembre(get_data_eem(), "inflacion", 2023)
+eem_data_diciembre <- function(
+    data_eem,
+    variable = "inflacion",
+    year_to_plot = 2023
+) {
+  checkmate::check_choice(variable, c(eem_details$variables$levels))
+  from_last_year <- data_eem |>
+    dplyr::filter(year == year_to_plot -1) |>
+    dplyr::select(
+      periodo,
+      matches("informante|colaborador"),
+      expectativa = .data[[paste0(variable, "_diciembre2")]]
+    )
+
+  data_eem |>
+    dplyr::filter(year == year_to_plot) |>
+    dplyr::select(
+      periodo,
+      matches("informante|colaborador"),
+      expectativa = .data[[paste0(variable, "_diciembre")]]
+    ) |>
+    dplyr::bind_rows(from_last_year)
+}
+
+#' Get EEM example data
+#'
+#' @param format wide or long format
+#'
+#' @return a data frame
+#' @export
+#'
+#' @examples
+#' example_eem_data("wide")
+#' example_eem_data("long")
+example_eem_data <- function(format = "wide") {
+  get_data_eem(
+    data_path = system.file("unnamed_eem.rds", package = "encuestasmacro"),
+    format = "wide"
+  )
 }
